c34152f301c8c44dd6a20bea2b272d8d124c0ebc
Yousef Rafat <81116377+yousef-rafat@users.noreply.github.com>
2026-02-14 15:21:20 +0200

    infer.py fix

--- src/infer.py
+++ src/infer.py
@@ -43,13 +43,23 @@ def general_generate_fn(model, inputs, eos_token, max_new_tokens = 50, temperatu
     generated_ids = inputs.clone()
     past_key_values = None
     sample = None # <- error silencer
+    embed_weight = F.normalize(model.embed_tokens.weight, p = 2, dim = -1)
+
     for i in range(max_new_tokens):
         current_inputs = inputs if i == 0 else sample
 
         logits, past_key_values = model(current_inputs,
-                       attention_mask = None,
-                       use_cache = True, past_key_values=past_key_values)
-        new_logit = logits[:, -1] if temperature == 1.0 else (logits[:, -1] / temperature)
+                       attention_mask = None, use_cache = True,
+                       past_key_values=past_key_values, return_hidden=True)
+
+        new_logit = logits[:, -1]
+        # normalization needed
+        new_logit = F.normalize(new_logit, p = 2, dim = -1)
+        new_logit = torch.matmul(new_logit, embed_weight.t()) * 20
+
+        if temperature != 1.0:
+            new_logit /= temperature
+
         probs = F.softmax(new_logit, dim = -1)
         sample = torch.multinomial(probs, num_samples = 1)
         generated_ids = torch.cat([generated_ids, sample], dim = -1)
